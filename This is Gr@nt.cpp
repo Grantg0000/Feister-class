f412368aaa4c15ddbba987e4b001bfe8e7bb89d7
Grantg0000 <97857818+Grantg0000@users.noreply.github.com>
2026-09-10 19:18:12 -0700

    Create This is Gr@nt.cpp

---
+++ This is Gr@nt.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+//Hello Prof. Feister
+int main()
+{
+   std::cout << "\033[1mThis is Grant\033[0m\n";
+   return 0;
+}
+
+//I'm sorry for being late.
+
+/* This is Grant.
+   I have made it bold to get mhy point across.
+   It's really hot outside. */
